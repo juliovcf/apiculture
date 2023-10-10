6eb5739a9f32a2cac2068cb626cde9ff0246707b
Julio <jperecab@mercadona.es>
2023-10-10 13:07:57 +0200

    created and update date in transaction

--- src/main/java/com/apiculture/main/model/Transaction.java
+++ src/main/java/com/apiculture/main/model/Transaction.java
@@ -1,16 +1,24 @@
 package com.apiculture.main.model;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import lombok.Data;
 
 @Data
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class Transaction {
 
@@ -34,6 +42,14 @@ public class Transaction {
   private User user;
 
   private String description;
+
+  @Column(updatable = false)
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  @Column(insertable = false)
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
 
   private boolean completed;
 
