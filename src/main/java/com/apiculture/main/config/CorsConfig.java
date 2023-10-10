96dfa5944f630d8d5198fcc3612941a36140529f
Julio <jperecab@mercadona.es>
2023-10-10 17:09:32 +0200

    corsConfig

--- src/main/java/com/apiculture/main/config/CorsConfig.java
+++ src/main/java/com/apiculture/main/config/CorsConfig.java
@@ -6,15 +6,14 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc // Esto es necesario en versiones más recientes de Spring Boot
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**") // Permite solicitudes desde cualquier origen
-        .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
-        .allowedHeaders("*") // Cabeceras permitidas
-        .allowCredentials(false) // No permite credenciales (por defecto)
-        .maxAge(3600); // Tiempo máximo de caché de preflights (en segundos)
+    registry.addMapping("/**")
+        .allowedHeaders("*")
+        .allowedOrigins("*")
+        .allowedMethods("*");
   }
 }
